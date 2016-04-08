293522510a9bd9dbdb71fe0280855d34412796db
Arthur Lara <avlara@hotmail.com>
2016-04-08 17:35:51 -0300

    lendo adequadamente os topicos no tempo do controlador

--- tilt_controller/src/main.cpp
+++ tilt_controller/src/main.cpp
@@ -8,6 +8,8 @@
 #include "tilt_srv/Range_srv.h"
 #include "tilt_srv/Sensor_srv.h"
 #include "tilt_srv/Ref.h"
+#include "tilt_srv/States.h"
+#include "std_msgs/String.h"
 
 // variáveis globais
 struct sigaction sa;
@@ -47,12 +49,24 @@ ros::Publisher thrustdir_pub;
 ros::Publisher thrustesq_pub;
 ros::Publisher servodir_pub;
 ros::Publisher servoesq_pub;
+ros::Publisher Step_pub;
+
+//subscribers
+ros::Subscriber X_sub;
+
+tilt_srv::States msg2;
+
+void X_Callback(tilt_srv::States msg)
+{
+	//std::cout << msg.header << std::endl;
+	msg2 = msg;
+}
 
 // temporização
 void timer_handler (int signum)
 {
 	// lendo sensores	
-	if (!imu.call(values_imu)) ROS_ERROR("Failed to call service IMU");
+	/*if (!imu.call(values_imu)) ROS_ERROR("Failed to call service IMU");
 	if (!gps_fix.call(values_fix_gps)) ROS_ERROR("Failed to call service fix GPS");
 	if (!gps_velocity.call(values_velocity_gps)) ROS_ERROR("Failed to call service velocity GPS");
 	if (!magnetic.call(values_magnetic)) ROS_ERROR("Failed to call service magnetic");
@@ -60,21 +74,26 @@ void timer_handler (int signum)
 	if (!servo_esq_srv.call(values_servo_esq)) ROS_ERROR("Failed to call service servo dir");
 	if (!servo_dir_srv.call(values_servo_dir)) ROS_ERROR("Failed to call service servo left");
 	if (!thrust_esq_srv.call(values_thrust_esq)) ROS_ERROR("Failed to call service thrust left");
-	if (!thrust_dir_srv.call(values_thrust_dir)) ROS_ERROR("Failed to call service thrusr dir");
+	if (!thrust_dir_srv.call(values_thrust_dir)) ROS_ERROR("Failed to call service thrusr dir");*/
+	std::cout << msg2.header << std::endl;
 	
-
-
 	// controlador + filtro
- 	thrustdir.ref = 40;
+ 	/*thrustdir.ref = 40;
 	thrustesq.ref = 40;
 	servodir.ref = 0;
-	servoesq.ref = 0;	
+	servoesq.ref = 0;*/	
 
 	// Atuadores
-	thrustdir_pub.publish(thrustdir);
+	/*thrustdir_pub.publish(thrustdir);
 	thrustesq_pub.publish(thrustesq);
 	servodir_pub.publish(servodir);
-	servoesq_pub.publish(servoesq);
+	servoesq_pub.publish(servoesq);*/
+	std_msgs::String msgpub;
+    	std::stringstream ss;
+    	ss << "hello world " ;
+    	msgpub.data = ss.str();
+	Step_pub.publish(msgpub);
+	//std::cout << "Entrei" <<  std::endl;	
 }
 
 void SetSampleTime(int n) // em ms
@@ -100,7 +119,7 @@ int main (int argc, char **argv)
 	ros::NodeHandle n;
 	
 	// declarando clientes
-	imu = n.serviceClient<tilt_srv::Imu_srv>("ValoresIMU");
+	/*imu = n.serviceClient<tilt_srv::Imu_srv>("ValoresIMU");
 	gps_fix = n.serviceClient<tilt_srv::NavSatFix_srv>("FixGPS");
 	gps_velocity = n.serviceClient<tilt_srv::Vector3Stamped_srv>("VelocityGPS");
 	magnetic = n.serviceClient<tilt_srv::Vector3Stamped_srv>("ValoresMagnetometro");
@@ -108,19 +127,27 @@ int main (int argc, char **argv)
 	servo_esq_srv = n.serviceClient<tilt_srv::Sensor_srv>("/Tilt/SensoraL/ValoresMotores");
 	servo_dir_srv = n.serviceClient<tilt_srv::Sensor_srv>("/Tilt/SensoraR/ValoresMotores");
 	thrust_esq_srv = n.serviceClient<tilt_srv::Sensor_srv>("/Tilt/SensorThrustesq/ValoresMotores");
-	thrust_dir_srv = n.serviceClient<tilt_srv::Sensor_srv>("/Tilt/SensorThrustdir/ValoresMotores");
+	thrust_dir_srv = n.serviceClient<tilt_srv::Sensor_srv>("/Tilt/SensorThrustdir/ValoresMotores");*/
 
 	// declarando publicadores
-	thrustdir_pub = n.advertise<tilt_srv::Ref>("/Tilt/RefThrustdir", 10);
+	/*thrustdir_pub = n.advertise<tilt_srv::Ref>("/Tilt/RefThrustdir", 10);
 	thrustesq_pub = n.advertise<tilt_srv::Ref>("/Tilt/RefThrustesq", 10);
 	servodir_pub = n.advertise<tilt_srv::Ref>("/Tilt/RefaL", 10);
-	servoesq_pub = n.advertise<tilt_srv::Ref>("/Tilt/RefaR", 10);	
+	servoesq_pub = n.advertise<tilt_srv::Ref>("/Tilt/RefaR", 10);*/	
+	Step_pub = n.advertise<std_msgs::String>("Step", 1);	
+
+	// declarando subscribers
+	//ros::Subscriber sub = n.subscribe("/Tilt/Teste", 1, X_Callback);
+	ros::Subscriber sub = n.subscribe("/Tilt/Teste", 1, X_Callback);
 
 	// configurando período de amostragem
 	SetSampleTime(250);
 	
 	// controlador funciona enquanto o usuário não pressionar a tecla ENTER
-	while(true);
+	while(true)
+	{
+	    ros::spinOnce();
+	}
 
 	return 0;
 }
